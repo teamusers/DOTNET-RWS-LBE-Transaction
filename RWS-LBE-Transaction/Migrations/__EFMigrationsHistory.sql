0d7de15c0af604d67be6fa38b971a9398d15dbbc
eric Gan <eric_gan0513@hotmail.com>
2025-06-16 13:09:36 +0800

    Feat: add migration history script. To be run in SIT environments

---
+++ RWS-LBE-Transaction/Migrations/__EFMigrationsHistory.sql
@@ -0,0 +1,20 @@
+﻿USE [LBE]
+GO
+
+CREATE TABLE [dbo].[__EFMigrationsHistory](
+ [MigrationId] [nvarchar](150) NOT NULL,
+ [ProductVersion] [nvarchar](32) NOT NULL,
+ CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY CLUSTERED 
+(
+ [MigrationId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+INSERT INTO [dbo].[__EFMigrationsHistory]
+           ([MigrationId]
+           ,[ProductVersion])
+     VALUES
+           ('20250616030249_AddMissingChanges',
+           '9.0.6') 
